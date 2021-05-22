b0c286deabffa69d367317e250c38b2f8ef648f2
JoseLopez07 <82963422+JoseLopez07@users.noreply.github.com>
2021-05-22 15:41:39 -0500

    Update issues.js

--- src/routes/api/issues.js
+++ src/routes/api/issues.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const db = require('../../db');
+const verifyToken = require('../../middleware/verifyToken');
+const verifyAdminToken = require('../../middleware/verifyAdminToken');
+const verifyParams = require('../../middleware/verifyParams');
+const { RequestError } = require('mssql');
 
+// used for generating the salt to encrypt passwords
+const SALT_ROUNDS = 10;
+
+const router = express.Router();
+
+router.get('/', getCountIssuesFromMonth);
+
+async function getCountIssuesFromMonth(req, res, next){
+    try {
+        const cant = await db.getIssuesFromMonth(req);
+        return res.send(cant);
+    } catch(err) {
+        return next(err);
+}
