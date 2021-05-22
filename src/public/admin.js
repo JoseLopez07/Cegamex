70f6f901926c081fa964624ea142aa57ca376f82
JoseLopez07 <82963422+JoseLopez07@users.noreply.github.com>
2021-05-22 13:09:22 -0500

    Update admin.js

--- src/public/admin.js
+++ src/public/admin.js
@@ -2,20 +2,19 @@ import api from '/modules/api.mjs';
 
 const apiClient = new api.ApiClient();
 const chKpi1 = documente.getElementById("ChartKpi1").getContext("2d");
-
-/*Prueba de que funcionen los chars*/
-const data = [2,5,6]
+const data = [2,5,6];
 const chart1 = new Chart(chKpi1, {
-  type = "bar"
-  data {
-    labels: data,
-    datasets: [
-      {
-      label: ["Enero", "Febrero", "Marzo"],
-      backgroundColor: "rgba(0,255,0,0,2)",
-      borderColor: "rgb(255,0,0)",
-      data: data,
-      },
-    ],
-  },
+    type = "bar",
+    data: {
+        labels: data,
+        datasets: [
+            {
+            label: ["Enero", "Febrero", "Marzo"],
+            backgroundColor: "rgba(0,255,0,0,2)",
+            borderColor: "rgb(255,0,0)",
+            data: data,
+            },
+            ],
+        },
+    },
 )
