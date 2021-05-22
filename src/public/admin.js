c39c0bdf15340ba05a4c2c227bc587c198c16432
JoseLopez07 <82963422+JoseLopez07@users.noreply.github.com>
2021-05-22 17:12:41 -0500

    Update admin.js

--- src/public/admin.js
+++ src/public/admin.js
@@ -1,8 +1,8 @@
 import api from '/modules/api.mjs';
 const apiClient = new api.ApiClient();
 let chKpi1 = document.getElementById("ChartKpi1").getContext("2d");
-const marzo = await apiClient.getCountIssuesFromMonth(3);
-console.log(marzo);
+const fechas = await apiClient.getFechasIssues();
+console.log(fechas);
 const data = [12,47,10];
 const chart1 = new Chart(chKpi1, {
     type: "bar",
