c15550ae5f843ece14ae21e14d8c6e6bba4b328d
Adrian S <adriansalmar@gmail.com>
2021-05-22 20:56:05 -0600

    correcciones a API e intentar implementación de test básico

--- src/routes/api/issues.js
+++ src/routes/api/issues.js
@@ -1,22 +1,18 @@
 const express = require('express');
-const bcrypt = require('bcryptjs');
 const db = require('../../db');
 const verifyToken = require('../../middleware/verifyToken');
-const verifyAdminToken = require('../../middleware/verifyAdminToken');
-const verifyParams = require('../../middleware/verifyParams');
-const { RequestError } = require('mssql');
-
-// used for generating the salt to encrypt passwords
-const SALT_ROUNDS = 10;
 
 const router = express.Router();
 
-router.get('/', getFechaIssues);
+router.get('/', verifyToken, getFechasIssues);
 
-async function getFechasIssues(req, res, next){
+async function getFechasIssues(req, res, next) {
     try {
-        const fechas = await db.getFechaIssue();
+        const fechas = await db.getFechasIssue();
         return res.send(fechas);
-    } catch(err) {
+    } catch (err) {
         return next(err);
+    }
 }
+
+module.exports = router;
