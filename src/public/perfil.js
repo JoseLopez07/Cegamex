eb11f7ecdf986b3fa5fbd2997ec750a83498eeaa
aletuls <66858756+aletuls@users.noreply.github.com>
2021-05-25 15:22:19 -0600

    Trofeos (logros) para cada usuario

--- src/public/perfil.js
+++ src/public/perfil.js
@@ -9,9 +9,10 @@ import api from '/modules/api.mjs';
     let twitterAccount = document.getElementById('user-twitter-acc');
     let position = document.getElementById('user-position');
     let trophies = document.getElementById('trophies');
+    let trophiesSummary = document.getElementById('trophies-summary');
 
     const userData = await (await apiClient.getUserData()).json();
-    const achievements = await (await apiClient.getAchievements()).json(); 
+    const achievements = await (await apiClient.getAchievements()).json();
 
     nameProfile.innerText = userData.firstName + ' ' + userData.lastName;
     email.innerText = userData.email;
@@ -19,7 +20,32 @@ import api from '/modules/api.mjs';
     position.innerText = userData.companyRole;
     profileImage[0].src = userData.picture;
 
+    [].forEach.call(achievements,function(a){
+        let newAchievementContainer = document.createElement('div');
+        let newAchievementContainerSummary = document.createElement('div');
+        let newAchievementImage = document.createElement('img');
+        let newAchievementImageSummary = document.createElement('img');
+        let newAchievementName = document.createElement('div');
+
+        newAchievementContainer.classList.add('text-center','col-auto', 'mx-auto');
+        newAchievementContainerSummary.classList.add('margin-trophy');
+        newAchievementImage.classList.add('trofeo');
+        newAchievementImage.src = 'imagenes\\trofeo.png';
+        newAchievementImageSummary.classList.add('trofeo');
+        newAchievementImageSummary.src = 'imagenes\\trofeo.png';
+        newAchievementName.classList.add('texto-normal-pequeno', 'mt-1');
+        newAchievementName.innerText = a.name;
+
+        newAchievementContainer.appendChild(newAchievementImage);
+        newAchievementContainer.appendChild(newAchievementName);
+        trophies.appendChild(newAchievementContainer);
+
+        newAchievementContainerSummary.appendChild(newAchievementImageSummary);
+        trophiesSummary.appendChild(newAchievementContainerSummary);
+    });    
+
     console.log(userData);
     console.log(achievements);
+    console.log(gameData)
 
 })();
