73a38fd7cbb3b17c179154699e5b37f37c06a007
JoseLopez07 <82963422+JoseLopez07@users.noreply.github.com>
2021-05-26 18:18:20 -0500

    Update admin.js
    Se agregaron los datos de la base de datos a la grafica de issues completadas por mes

--- src/public/admin.js
+++ src/public/admin.js
@@ -1,9 +1,8 @@
 import api from '/modules/api.mjs';
 const apiClient = new api.ApiClient();
 let chKpi1 = document.getElementById("ChartKpi1").getContext("2d");
-const fechas = await apiClient.getFechasIssues();
-console.log(fechas);
-const data = [12,47,10];
+const countsfechas = await (await apiClient.getCountFechasIssues()).json();
+const data = [countsfechas[0].enero,countsfechas[0].febrero,countsfechas[0].marzo];
 const chart1 = new Chart(chKpi1, {
     type: "bar",
     data: {
