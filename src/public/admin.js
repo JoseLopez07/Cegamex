d1f55276b2fb830f10ba467e6e2668f7e9272e6e
JoseLopez07 <82963422+JoseLopez07@users.noreply.github.com>
2021-05-27 15:20:46 -0500

    Update admin.js

--- src/public/admin.js
+++ src/public/admin.js
@@ -3,8 +3,6 @@ const apiClient = new api.ApiClient();
 let chKpi1 = document.getElementById("ChartKpi1").getContext("2d");
 let chKpi2 = document.getElementById("ChartKpi2").getContext("2d");
 const countsfechas = await (await apiClient.getCountFechasIssues()).json();
-const userData = await (await apiClient.getUserData()).json();
-const gameData = await (await apiClient.getPetData()).json();
 
 console.log(userData);
 const dataEnero = countsfechas[0].enero;
