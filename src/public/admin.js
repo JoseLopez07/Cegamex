4de9182f29d49eb9558126ff317cb138e4de93d2
JoseLopez07 <82963422+JoseLopez07@users.noreply.github.com>
2021-05-27 15:15:50 -0500

    Update admin.js

--- src/public/admin.js
+++ src/public/admin.js
@@ -1,8 +1,19 @@
 import api from '/modules/api.mjs';
 const apiClient = new api.ApiClient();
 let chKpi1 = document.getElementById("ChartKpi1").getContext("2d");
+let chKpi2 = document.getElementById("ChartKpi2").getContext("2d");
 const countsfechas = await (await apiClient.getCountFechasIssues()).json();
-const data = [countsfechas[0].enero,countsfechas[0].febrero,countsfechas[0].marzo];
+const userData = await (await apiClient.getUserData()).json();
+const gameData = await (await apiClient.getPetData()).json();
+
+console.log(userData);
+const dataEnero = countsfechas[0].enero;
+const dataFebrero = countsfechas[0].febrero;
+const dataMarzo = countsfechas[0].marzo;
+const data = [dataEnero,dataFebrero,dataMarzo];
+const dataResolve = dataEnero+dataFebrero+dataMarzo;
+const dataNotResolve = countsfechas[0].NULLS;
+const data2 = [dataResolve, dataNotResolve];
 const chart1 = new Chart(chKpi1, {
     type: "bar",
     data: {
@@ -10,6 +21,7 @@ const chart1 = new Chart(chKpi1, {
         datasets: [
             {
             label: "Issues Resueltas",
+            //Color de las barras
             backgroundColor: "rgba(0,255,0,0.2)",
             borderColor: "rgb(255,0,0)",
             data: data,
@@ -17,4 +29,23 @@ const chart1 = new Chart(chKpi1, {
             ],
         },
         options: {},
-    });
+    }
+);
+
+const chart2 = new Chart(chKpi2, {
+    type: "doughnut",
+    data: {
+        labels: ["Resueltas", "Sin resolver"],
+        datasets: [
+            {
+            label: "Issues Resueltas vs sin resolver",
+            //Color de la rueda ["color1", "color2"]
+            backgroundColor: ["rgba(0,255,0,0.2)", "red"],
+            borderColor: "rgb(255,0,0)",
+            data: data2,
+            },
+            ],
+        },
+        options: {},
+    }
+);
