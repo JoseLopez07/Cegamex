dd8f38b8e331934db325535f7d4a854b4256ddf5
aletuls <66858756+aletuls@users.noreply.github.com>
2021-06-07 13:53:58 -0600

    reload pagina de juego de acuerdo al token

--- src/public/juego.js
+++ src/public/juego.js
@@ -1,4 +1,20 @@
 import utils from '/modules/utils.mjs';
+import api from '/modules/api.mjs';
+const apiClient = new api.ApiClient();
+
+const parseJwt = (token) => {
+    try {
+      return JSON.parse(atob(token.split('.')[1]));
+    } catch (e) {
+      return null;
+    }
+  };
+
+let expTime = parseJwt(apiClient.token).exp;
+
+setTimeout(function() {
+    location.reload();
+}, (expTime-60000));
 
 utils.searchUser();
 utils.focusSearchInput();
